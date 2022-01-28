d9081d203b0a6c4397d3ebffca96904292711e85
Bhavesh Lulla <65904865+bhavi133@users.noreply.github.com>
2022-01-28 19:46:33 +0530

    Create Set Intersection Operation.py

---
+++ Sets/Set Intersection Operation.py
@@ -0,0 +1,7 @@
+Link : https://www.hackerrank.com/challenges/py-set-intersection-operation/problem
+
+n = int(input())
+n = set(map(int, input().split()))
+b = int(input())
+b = set(map(int, input().split()))
+print(len(n&b))
