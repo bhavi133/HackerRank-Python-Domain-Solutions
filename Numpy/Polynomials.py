2a7366f3d332c9e4316bf4ea0d305b3ea0398828
Bhavesh Lulla <65904865+bhavi133@users.noreply.github.com>
2022-02-01 12:41:43 +0530

    Create Polynomials.py

---
+++ Numpy/Polynomials.py
@@ -0,0 +1,7 @@
+Link : https://www.hackerrank.com/challenges/np-polynomials/problem
+
+import numpy
+
+p = numpy.array(list(map(float,input().split())),float)
+x = float(input())
+print(numpy.polyval(p, x))
