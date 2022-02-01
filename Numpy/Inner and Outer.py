6ca34c6446bf649f83667fd6088af881248965fc
Bhavesh Lulla <65904865+bhavi133@users.noreply.github.com>
2022-02-01 12:39:51 +0530

    Create Inner and Outer.py

---
+++ Numpy/Inner and Outer.py
@@ -0,0 +1,8 @@
+Link : https://www.hackerrank.com/challenges/np-inner-and-outer/problem?isFullScreen=true
+
+import numpy
+
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+print(numpy.inner(A, B))
+print(numpy.outer(A, B))
