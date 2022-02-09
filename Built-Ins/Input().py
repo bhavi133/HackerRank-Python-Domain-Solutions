3474428942996674cc4ea0b9d83f54c9deca75d2
Bhavesh Lulla <65904865+bhavi133@users.noreply.github.com>
2022-02-09 17:03:54 +0530

    Create Input().py

---
+++ Built-Ins/Input().py
@@ -0,0 +1,6 @@
+Link : https://www.hackerrank.com/challenges/input/problem
+
+if __name__ == "__main__":
+    x, k = map(int, input().strip().split())
+    equation = input().strip()
+    print(eval(equation) == k)
