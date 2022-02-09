30eb62451fb3f49b6776c2272b8e0ef044af73b7
Bhavesh Lulla <65904865+bhavi133@users.noreply.github.com>
2022-02-09 15:54:24 +0530

    Create Mutations.py

---
+++ Strings/Mutations.py
@@ -0,0 +1,12 @@
+Link : https://www.hackerrank.com/challenges/python-mutations/problem
+
+def mutate_string(string, position, character):
+    char = list(string)
+    char[position] = character
+    return "".join(char)
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
