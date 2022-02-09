562d700db5306d13f4d3a5c5bc6afd129ff0d6ef
Bhavesh Lulla <65904865+bhavi133@users.noreply.github.com>
2022-02-09 16:14:00 +0530

    Create Set .union() Operation.py

---
+++ Sets/Set .union() Operation.py
@@ -0,0 +1,7 @@
+Link : https://www.hackerrank.com/challenges/py-set-union/problem
+
+n1 = int(input())
+set1 = set(map(int, input().split()))
+n2 = int(input())
+set2 = set(map(int, input().split()))
+print(len(set1.union(set2))
